e5a6b167c7778b6edc0dcbc054fb479322ec4ce8
Rahmayani <120942133+EpingMDZ@users.noreply.github.com>
2023-11-24 08:04:47 +0800

    commit Sitti Rahmayani Ose

---
+++ beli.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include('koneksi.php');
+$id = $_GET['id'];
+if (isset($_SESSION['keranjang'][$id])) {
+    $_SESSION['keranjang'][$id] += 1;
+} else {
+    $_SESSION['keranjang'][$id] = 1;
+}
+//echo "<pre>";
+//print_r($_SESSION);
+//echo "</pre>";
+//echo "<pre>";
+//print_r($_SESSION);
+//echo "</pre>";
+echo "<script>alert('produk telah masuk ke keranjang belanja')</script>";
+echo "<script>location='cart.php'</script>";
+echo "<script></script>";
