b1d026089b849cd058c2c3f196b9166c98f84200
Nurdian12345 <nurdiankasim1948@gmail.com>
2023-12-01 07:57:50 +0800

    Nurdian Kasim

--- admin/user.php
+++ admin/user.php
@@ -1,6 +1,51 @@
 <?php
+
+class User
+{
+    private $koneksi;
+
+    public function __construct($koneksi)
+    {
+        $this->koneksi = $koneksi;
+    }
+
+    public function getAllUsers()
+    {
+        $query = "SELECT * FROM tb_user";
+        $no = 1;
+        $sql = mysqli_query($this->koneksi, $query);
+
+        $users = [];
+
+        while ($data = mysqli_fetch_array($sql)) {
+            $users[] = $data;
+            $no++;
+        }
+
+        return $users;
+    }
+
+    public function deleteUser($id)
+    {
+        // Implementasi kode untuk menghapus user berdasarkan ID
+        $query = "DELETE FROM tb_user WHERE id = $id";
+        mysqli_query($this->koneksi, $query);
+    }
+}
+
 include '../koneksi.php';
+
+$userObj = new User($koneksi);
+$users = $userObj->getAllUsers();
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['halaman']) && $_GET['halaman'] == 'hapususer' && isset($_GET['id'])) {
+    // Hapus user berdasarkan ID jika permintaan datang dari halaman hapususer
+    $userObj->deleteUser($_GET['id']);
+    header('Location: index.php'); // Redirect kembali ke halaman utama setelah penghapusan
+    exit();
+}
 ?>
+
 <h2>Data User</h2>
 <a href="index.php?halaman=tambahuser" class="btn btn-outline-primary">Tambah Data</a><br>
 <table class="table table-bordered table-hover mt-3">
@@ -10,23 +55,15 @@ include '../koneksi.php';
         <td>email</td>
         <td>aksi</td>
     </tr>
-    <?php
-    $query = "SELECT*FROM tb_user";
-    $no = 1;
-    $sql = mysqli_query($koneksi, $query);
-    while ($data = mysqli_fetch_array($sql)) {
-
-    ?>
+    <?php foreach ($users as $user) : ?>
         <tr>
-            <td><?php echo $no ?></td>
-            <td><?php echo $data['fullname'] ?></td>
-            <td><?php echo $data['email'] ?></td>
+            <td><?php echo $user['id'] ?></td>
+            <td><?php echo $user['fullname'] ?></td>
+            <td><?php echo $user['email'] ?></td>
             <td>
-                <a href="index.php?halaman=edituser&id=<?php echo $data['id'] ?>" class="btn btn-outline-success">Edit</a>
-                <a href="index.php?halaman=hapususer&id=<?php echo $data['id'] ?>" class="btn btn-outline-danger">Hapus</a>
+                <a href="index.php?halaman=edituser&id=<?php echo $user['id'] ?>" class="btn btn-outline-success">Edit</a>
+                <a href="index.php?halaman=hapususer&id=<?php echo $user['id'] ?>" class="btn btn-outline-danger" onclick="return confirm('Apakah Anda yakin ingin menghapus?')">Hapus</a>
             </td>
         </tr>
-    <?php
-        $no++;
-    } ?>
+    <?php endforeach; ?>
 </table>
